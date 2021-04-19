cb8d353aef5df30234e163a199cf5fafecba893e
Brandon DeRosier <x@bdero.me>
2021-04-18 18:29:23 -0700

    Add graphics for server and node

--- lib/netwalk_graphics.dart
+++ lib/netwalk_graphics.dart
@@ -59,21 +59,83 @@ class NetwalkGraphics {
   }
 
   void paintAtlas(Canvas canvas) {
-    final paint = Paint();
-    paint.color = Colors.white;
+    {
+      var paint = Paint()..color = Colors.white;
+      // Piece graphics
+      canvas.save();
+      {
+        canvas.translate(atlasSize / 2, atlasSize / 2);
+        NetwalkPiece.values.forEach((v) {
+          canvas.drawPath(_pieces[v]!.paths[0], paint);
+          canvas.translate(atlasSize, 0);
+        });
+      }
+      canvas.restore();
+
+      // Lock graphics
+      canvas.save();
+      {
+        canvas.translate(atlasSize / 2, atlasSize * 1.5);
+        NetwalkPiece.values.forEach((v) {
+          canvas.drawPath(_pieces[v]!.lockPath, paint);
+          canvas.translate(atlasSize, 0);
+        });
+      }
+      canvas.restore();
+    }
+
     canvas.save();
-    canvas.translate(atlasSize / 2, atlasSize / 2);
-    NetwalkPiece.values.forEach((v) {
-      canvas.drawPath(_pieces[v]!.paths[0], paint);
+    {
+      // Server graphic
+      canvas.translate(atlasSize / 2, atlasSize * 2.5);
+      {
+        var paint = Paint()
+          ..strokeCap = StrokeCap.round
+          ..strokeWidth = atlasSize * 0.2;
+        var width = atlasSize * 0.3;
+        var vOffset = atlasSize * 0.26;
+        var radius = atlasSize * 0.045;
+        for (var i = 0; i < 3; i++) {
+          var lineY = -vOffset + i * vOffset;
+          paint.color = Colors.black;
+          canvas.drawLine(Offset(-width, lineY), Offset(width, lineY), paint);
+          paint.color = Colors.white;
+          canvas.drawCircle(Offset(width, lineY), radius, paint);
+        }
+      }
+
+      // Node graphics
       canvas.translate(atlasSize, 0);
-    });
-    canvas.restore();
-    canvas.save();
-    canvas.translate(atlasSize / 2, atlasSize * 1.5);
-    NetwalkPiece.values.forEach((v) {
-      canvas.drawPath(_pieces[v]!.lockPath, paint);
-      canvas.translate(atlasSize, 0);
-    });
+      {
+        var paint = Paint()
+          ..color = Colors.black
+          ..strokeCap = StrokeCap.round
+          ..strokeWidth = atlasSize * 0.2;
+        var width = atlasSize * 0.35;
+        var height = atlasSize * 0.25;
+        var radius = atlasSize * 0.05;
+        canvas.drawRRect(
+            RRect.fromRectAndRadius(
+                Rect.fromLTRB(-width, -height, width, height),
+                Radius.circular(radius * 2)),
+            paint);
+
+        canvas.translate(atlasSize, 0);
+        canvas.drawRRect(
+            RRect.fromRectAndRadius(
+                Rect.fromLTRB(-width, -height, width, height),
+                Radius.circular(radius * 2)),
+            paint);
+        paint.color = Colors.white;
+        var bezel = atlasSize * 0.07;
+        canvas.drawRRect(
+            RRect.fromRectAndRadius(
+                Rect.fromLTRB(-width + bezel, -height + bezel, width - bezel,
+                    height - bezel),
+                Radius.circular(radius * 2 - bezel * 0.8)),
+            paint);
+      }
+    }
     canvas.restore();
   }
 
